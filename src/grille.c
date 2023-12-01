43cd86612ff564021e73a01b3b8435f64811db4c
orfao <marcoorfao2003@gmail.com>
2023-12-01 15:59:01 +0100

    maj

---
+++ src/grille.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <graph.h>
+
+int* creer_grille(void){
+    int* Grille=NULL;
+    Grille=(int*) malloc(40*60*sizeof(int));
+    return Grille;
+}
+
+void creer_grille_graph(){
+    int x_grille=0,y_grille=0,x,y;
+    ChoisirCouleurDessin(CouleurParComposante(0,0,0));
+    DessinerRectangle(x_grille,y_grille,1200,1000);
+    DessinerRectangle(x_grille,y_grille,900,600);
+    for(x=x_grille;x<1100;x=x+15){
+        DessinerSegment(x,y_grille,x,800);
+    }
+    for(y=y_grille;y<800;y=y+15){
+        DessinerSegment(x_grille,y,1100,y);
+    }
+
+}
+
+int main(void){
+    char x;
+    InitialiserGraphique();
+    CreerFenetre(0,0,1300,1300);
+    creer_grille_graph();
+    while(x != 'q'){
+        scanf("%c",&x);
+    }
+}
