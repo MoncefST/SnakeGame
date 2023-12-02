438547e390001ee1b60cb77b4eeb56dd11181813
orfao <marcoorfao2003@gmail.com>
2023-12-02 17:21:07 +0100

    test

--- src/timer.c
+++ src/timer.c
@@ -1,28 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<graph.h>
-#define delta 1000000L
 
 void creer_timer(void){
-    int sec;
-    unsigned long temps;
-    temps=Microsecondes();
-    while(sec!=10){
-        temps=Microsecondes+delta;
-        printf("%d\n",sec);
-        sec=temps%11;
+    int sec = 0,mil_sec=0,min=0;
+    unsigned long temps = 0;
+    printf("temps : %lu\n",temps);
+    while(min<=2){
+        printf("[%d:%d,%d]\n",min,sec,mil_sec);
+        temps=Microsecondes();
+        printf("temps : %lu\n",temps);
+        sec=(temps/1000000)%60;
+        mil_sec=(temps/1000)%101;
+        min=(temps/60000000)%60;
+        printf("[%d:%d,%d]\n",min,sec,mil_sec);
     }
 }
 
-
-
-
-
-
-
 int main(void){
     InitialiserGraphique();
-    CreerFenetre(0,0,1300,1300);
     creer_timer();
     FermerGraphique();
+    return EXIT_SUCCESS;
 }
