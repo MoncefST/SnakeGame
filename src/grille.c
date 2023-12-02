4764cff85e3cd7543ba8811a99e49445303e2a9b
stiti <moncef.stiti@gmail.com>
2023-12-02 15:08:33 +0100

    Amélioration de grille.c

--- src/grille.c
+++ src/grille.c
@@ -1,47 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <graph.h>
 
-int* creer_grille(void){
-    int* Grille=NULL;
-    Grille=(int*) malloc(40*60*sizeof(int));
-    return Grille;
-}
+/*Dessine une grille de 60 colonnes sur 40 lignes*/
+void dessinerGrille() {
+    int i,j;
+    int tailleCase = 20; 
+    couleur couleur1 = CouleurParComposante(170, 215, 82); /*Vert clair*/
+    couleur couleur2 = CouleurParComposante(54, 103, 0); /*Vert foncé*/
 
-void creer_grille_graph(){
-<<<<<<< HEAD
-    int x_grille=0,y_grille=0,x,y;
-    ChoisirCouleurDessin(CouleurParComposante(0,0,0));
-    DessinerRectangle(x_grille,y_grille,1200,1000);
-    DessinerRectangle(x_grille,y_grille,900,600);
-    for(x=x_grille;x<1100;x=x+15){
-        DessinerSegment(x,y_grille,x,800);
-    }
-    for(y=y_grille;y<800;y=y+15){
-        DessinerSegment(x_grille,y,1100,y);
-=======
-    int x=200,y=200;
-    ChoisirCouleurDessin(CouleurParComposante(0,0,0));
-    DessinerRectangle(x,y,1200,1000);
-    DessinerRectangle(x,y,900,600);
-    for(x=200;x<1100;x=x+15){
-        DessinerSegment(x,y,x,800);
-    }
-    x=200;
-    y=200;
-    for(y=200;y<800;y=y+15){
-        DessinerSegment(x,y,1100,y);
->>>>>>> 03cdfc71722afd1d8631e3ed432af6d031cd9b7e
-    }
+    for (i = 0; i < 40; i++) {
+        for (j = 0; j < 60; j++) {
+            /*Alterner les couleurs en fonction de la case (1 case sur deux...)*/
+            couleur couleurCase = (i + j) % 2 == 0 ? couleur1 : couleur2;
 
-}
-
-int main(void){
-    char x;
-    InitialiserGraphique();
-    CreerFenetre(0,0,1300,1300);
-    creer_grille_graph();
-    while(x != 'q'){
-        scanf("%c",&x);
+            /*Dessiner la case*/
+            ChoisirCouleurDessin(couleurCase);
+            RemplirRectangle(j * tailleCase, i * tailleCase, tailleCase, tailleCase);
+        }
     }
 }
