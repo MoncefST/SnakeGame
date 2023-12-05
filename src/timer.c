dfb98b4b2f6bd1396f03558d35f1cf7de5f80265
orfao <marco.orfao@etu.u-pec.fr>
2023-12-05 21:22:05 +0100

    ajout test timer graphique

--- src/timer.c
+++ src/timer.c
@@ -2,16 +2,21 @@
 #include<stdlib.h>
 #include<graph.h>
 
+void afficher_timer(void){
+    EcrireTexte(0,0,coucou,2);
+}
+
 void creer_timer(void){
     int sec = 0,mil_sec=0,min=0;
     unsigned long temps = 0,t1=0;
     t1=Microsecondes();
-    while(sec<=12){
+    while(min<=12){
         printf("[%0.2d:%0.2d]\n",min,sec);
         temps=Microsecondes()-t1;
         min=(temps/60000000)%60;
         temps=temps%60000000;
         sec=(temps/1000000)%60;
+        EcrireTexte();
     }
 }
 
